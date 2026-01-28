4b0f793571432809f9969c8f1ac995b532e0a7dd
Naseem-gif <nk842931@gmail.com>
2026-01-28 22:27:00 +0500

    changes made

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 const input = document.querySelector("#todoInput");
 const ol = document.querySelector("ol");
-const todoArr = []
+const todoArr = [];
 
+const renderTodo = () => {
+  ol.innerHTML = "";
+  for (let i = 0; i < todoArr.length; i++) {
+    ol.innerHTML += `<li>${todoArr[i]}
+    <button onclick="editTodo(${i})">Edit Todo</button>
+    <button onclick="deleteTodo(${i})">Delete Todo</button>
+    </li>`;
+  }
+};
 
-function renderTodo(){
-    ol.innerHTML = ""
-    for (let i = 0; i < todoArr.length; i++){
-         ol.innerHTML += `<li>${todoArr[i]} <button onclick="deleteTodo(${i})">Delete</button> 
-         <button onclick="editTodo(${i})">Edit</button> </li>`
-         console.log(todoArr);
-         
-    }
+const addTodo = () => {
+  todoArr.push(input.value);
+  renderTodo();
+  input.value = "";
+};
+
+function deleteTodo(index) {
+  todoArr.splice(index, 1);
+  renderTodo()
 }
 
-function addTodo(){
- 
-    todoArr.push(input.value);
+
+function editTodo(index) {
+    todoArr.splice(index, 1, prompt("Edit Todo"));
     renderTodo()
 
-
-    input.value = ""  
 }
-
-
-
-function deleteTodo(index){
-    todoArr.splice(index, 1)
-    renderTodo()
-}
-
-
-function editTodo(index){
-    todoArr.splice(index, 1, prompt("Edit Todo"))
-    renderTodo()
-}
-
-
-
-
-
-
